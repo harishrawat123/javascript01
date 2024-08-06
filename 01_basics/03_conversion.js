fce296dbd0b343f634be27817486f2c3c2b5bec4
harishrawat123 <143165075+harishrawat123@users.noreply.github.com>
2024-08-06 10:48:04 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,28 @@
+let score="56"
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let value="56abc"
+console.log(typeof(value));
+
+let valueInValue=Number(value)
+console.log(typeof valueInValue);
+console.log(valueInValue);
+
+let a=null
+console.log(typeof(a));
+
+let valueIna=Number(a)
+console.log(typeof valueIna);
+console.log(valueIna);
+
+
+let b=undefined
+console.log(typeof(b));
+
+let valueInb=Number(b)
+console.log(typeof valueInb);
+console.log(valueInb);
