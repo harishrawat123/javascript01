e53f21091ec9327c25a19032d84a8c5c500f1775
harishrawat123 <143165075+harishrawat123@users.noreply.github.com>
2024-08-07 09:58:29 +0000

    string

---
+++ 01_basics/07_string.js
@@ -0,0 +1,21 @@
+const name = "harish"
+console.log(name[0])
+console.log(name.charAt(2));
+console.log(name.indexOf('r'));
+console.log(name.toUppercase);
+
+const newString=name.substring(0,4)
+console.log(newString);
+
+const newString1=name.substring(6,4)
+console.log(newString1);
+
+const newString2="rawat"
+console.log(newString2);
+console.log(newString2.trim());
+
+const url="https:harish.com/rajkumarpandu.com"
+console.log(url.replace('pandu', ''))
+console.log(url.includes('harish'));
+console.log(url.includes('rawat'))
+
