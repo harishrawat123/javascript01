24d8d871797b1838278e5a65da92aefce16f2cd8
harishrawat123 <143165075+harishrawat123@users.noreply.github.com>
2024-08-07 05:59:01 +0000

    done

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,21 @@
+let value=3
+let negValue=-value
+console.log(negValue)
+// + - * / %
+
+let str1="Hello"
+let str2=" World"
+console.log(str1+str2)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log(1+2+"2")
+console.log("1"+2+2)
+
+console.log(+true)
+
+let gamePoints=100
+gamePoints++
+console.log(gamePoints)
+++gamePoints
+console.log(gamePoints)
---
+++ 01_basics/06_datatypessummery.js
@@ -0,0 +1,3 @@
+const score=100
+const scorevalue=100.5
+const isLoggedIn=false
