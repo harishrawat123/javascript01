73999482898b0caadec8d3001f9f3ae89a181c42
harishrawat123 <143165075+harishrawat123@users.noreply.github.com>
2024-08-09 12:37:28 +0000

    Array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,41 @@
+// ARRAYS
+
+const myArray=[0,1,2,3,4,5]
+const myheros=["shaktiman", "superman"]
+const myArray2= new Array(1,5,5,6,4)
+// console.log(myArray);
+// console.log(myheros[1]);
+// console.log(myArray2[4]);
+
+//array methods
+
+// myArray.push(9, 4)
+// console.log(myArray)
+// myArray.pop()
+// console.log(myArray);
+
+// myArray.unshift(8)
+// console.log(myArray)
+// myArray.shift()
+// console.log(myArray);
+
+// console.log(myArray.includes(9))
+// console.log(myArray.indexOf(5));
+
+// const newArr=myArray.join()
+// console.log(newArr);
+// console.log(typeof newArr);
+
+//slice, splice
+
+console.log("A ", myArray);
+
+const mynA1=myArray.slice(1,3)
+console.log(mynA1)
+console.log("B", myArray)
+
+const mynA2=myArray.splice(1,3)
+console.log(myArray)
+
+console.log("C", myArray)
+
